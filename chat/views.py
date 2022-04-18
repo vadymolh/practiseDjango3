601cdc58e2e8cdbce7003c5e196aeaaf385a38c2
Vadym Olkhovyk <vadymolhovyk@gmail.com>
2022-04-18 20:14:51 +0300

    Bad chat

--- chat/views.py
+++ chat/views.py
@@ -21,11 +21,13 @@ def load_messages(request, pk):
     }
     return render(request, "chatroom.html", context)
 
+@login_required
 def load_messages_ajax(request, pk):
     other_user = get_object_or_404(User, pk=pk)
     messages = Message.objects.filter(seen=False, 
                                       receiver=request.user)
     message_list = []
+    print ("Messages:")
     for message in messages:
         message_list.append({"sender": message.sender.username,
                              "message": message.message,
@@ -34,16 +36,18 @@ def load_messages_ajax(request, pk):
         message.seen = True
         message.save()
     if request.method =="POST":
-        message = request.POST.get("message")
-        message = json.loads(message)
+        message = json.loads(request.body)
+        print (message)
         if message:
             m = Message.objects.create(
                 sender=request.user,
                 receiver=other_user,
                 message=message
             )
+            m.save()
             message_list.append(
                 {"sender": request.user.username,
+                "username": request.user.username,
                  "message": message,
                  "sent": True,
                  "date_created": naturaltime(m.date_created)}
